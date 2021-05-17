c56f49db7647876d6c0e4963b498391921a4553a
will-a-creator <68308538+will-a-creator@users.noreply.github.com>
2021-05-17 18:21:11 -0400

    Create file.js

---
+++ file.js
@@ -0,0 +1,38 @@
+//THEODINPROJECT 
+const add = function(a,b) {
+	return a + b;
+}
+
+const subtract = function (a,b) {
+	return a - b;
+}
+
+const sum = function(array) {
+	return array.reduce((total, current)=> total + current,0);
+}
+
+const multiply () = function(array){
+	return array.length? array.reduce((a, next) => a * next): 0;
+}
+
+const power = function(a,b) {
+	return Math.pow(a,b);
+}
+
+const factorial = function(n) {
+    if (n === 0) return 1;
+    let product = 1;
+    for (let i = n; i > 0; i--) {
+      product *= i;
+    }
+    return product;
+  };
+
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
